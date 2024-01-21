f76fbba26ca2141cc79d82d233260d5b8cf2d7dd
Sigurd Torve Sæther <torve_s@live.no>
2024-01-21 15:29:13 +0100

    All modules added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Protocols from "./modules/protocols/Protocols";
 import Filters from "./modules/filters/Filters";
+import Info from "./modules/info/Info"
 
 function App() {
   return (
     <>
       <Filters />
       <Protocols />
+      <Info/>
     </>
   );
 }
---
+++ src/modules/info/Info.tsx
@@ -0,0 +1,11 @@
+function Info() {
+  return (
+    <>
+      <h3>Protokoll</h3>
+      <h3>beskrivelse</h3>
+      <h3>info</h3>
+    </>
+  );
+}
+
+export default Info;
